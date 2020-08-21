a23ccb1c35978408c6d1b619b25f7f852ee9bb15
FatemeAfshar20 <uttab4760@gmail.com>
2020-08-20 23:02:01 -0700

    Optimize logic Program and handle configuration chang

--- app/src/main/java/com/example/rowsofcolor/MainActivity.java
+++ app/src/main/java/com/example/rowsofcolor/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.rowsofcolor;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -10,43 +10,29 @@ import android.widget.TextView;
 import java.lang.reflect.Field;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String BUNDLE_VIEW_VISIBILITY = "View Visibility";
     private int numTextView = 12;
-    private TextView[] mColorViews =new  TextView[numTextView];
-    private String[] mColorNameId = {"زردخردلی", "آبی فیروزه ای", "سبز روشن", "صورتی کثیف", "بنفش روشن"
-            , "بادمجانی", "صدفی", "یاسی", "آبی آسمانی", "قرمز"
-            , "نارنجی", "آبی"};
-    int[] resourceId =new int[numTextView];
+    private TextView[] mColorViews = new TextView[numTextView];
+    private int[] mColorName = {R.string.color_beauty_yellow, R.string.color_turquoise_blue, R.string.color_beauty_green, R.string.color_beauty_pink
+            , R.string.color_beauty_purple_light, R.string.color_brown,R.string.color_beauty_pink_light, R.string.color_yasi, R.string.color_beauty_blue
+            , R.string.color_beauty_red, R.string.color_orange,R.string.color_blue_dark};
+    int[] resourceId = createId(mColorViews, "box_");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        resourceId=createId(mColorViews,"box_");
         findElem();
         setListener();
+        saveInstance(savedInstanceState);
     }
 
     private <T extends View> int[] createId(T[] views, String commonPartOfId) {
         int[] IDs = new int[views.length];
         for (int i = 0; i < views.length; i++) {
-            int tempt = getId(commonPartOfId + i, R.id.class);
-            IDs[i] = tempt;
+            IDs[i] =  getId(commonPartOfId + i, R.id.class);
         }
         return IDs;
-    }
-
-    public void changeText(TextView textView) {
-        TextView[] textViews = mColorViews;
-        String text = (String) textView.getText();
-        for (int i = 0; i < mColorViews.length; i++) {
-            textViews[i].setText(text);
-        }
-    }
-
-    public void backDefText() {
-        for (int i = 0; i < mColorViews.length; i++) {
-            mColorViews[i].setText(mColorNameId[i]);
-        }
     }
 
     private void findElem() {
@@ -72,6 +58,41 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
+    }
+
+    public void changeText(TextView textViewListener) {
+        TextView[] textViews = mColorViews;
+        String text = (String) textViewListener.getText();
+        for (int i = 0; i < mColorViews.length; i++) {
+            textViews[i].setText(text);
+        }
+    }
+// back Definition text
+    public void backDefText() {
+        for (int i = 0; i < mColorViews.length; i++) {
+            mColorViews[i].setText(mColorName[i]);
+        }
+    }
+
+    public void saveInstance(Bundle bundle) {
+        if (bundle != null) {
+            int[] setViewVisibility=bundle.getIntArray(BUNDLE_VIEW_VISIBILITY);
+            for (int i = 0; i < setViewVisibility.length; i++) {
+                mColorViews[i].setVisibility(setViewVisibility[i]);
+            }
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        int[] txtViewVisibility = new int[numTextView];
+
+        for (int i = 0; i < txtViewVisibility.length; i++) {
+            txtViewVisibility[i] = mColorViews[i].getVisibility();
+        }
+
+        outState.putIntArray(BUNDLE_VIEW_VISIBILITY, txtViewVisibility);
     }
 
     public static int getId(String resourceName, Class<?> c) {
